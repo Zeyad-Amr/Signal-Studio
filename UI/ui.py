22cac5607227825c0a48b9073f067f983f8820f7
Kamel Mohamed <112772763+Kamel-Mohammed@users.noreply.github.com>
2022-10-14 16:51:38 +0200

    feat: removing streamlit hamburger and footer.

--- UI/ui.py
+++ UI/ui.py
@@ -4,3 +4,15 @@ class AppUi:
     def __init__(self):
         st.set_page_config(page_title='Sampling Studio')
         st.title("Coming Soon...")
+
+        # Removing Streamlit hamburger and footer.
+        st.markdown("""
+        <style>
+            .css-9s5bis.edgvbvh3 {
+                visibility : hidden;
+            }
+            .css-1q1n0ol.egzxvld0 {
+                visibility : hidden;
+            }
+        </style>
+        """, unsafe_allow_html=True)
