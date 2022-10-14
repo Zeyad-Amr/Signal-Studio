3c7b808de3cf9c4b7b25517bc9b8c741466e0338
Kamel Mohamed <112772763+Kamel-Mohammed@users.noreply.github.com>
2022-10-15 00:52:33 +0200

    feat: customize download/upload buttons and error messages

--- UI/ui.py
+++ UI/ui.py
@@ -1,8 +1,10 @@
+import dis
 import os
 from pathlib import Path
 import streamlit as st
+import plotly.figure_factory as ff
 from werkzeug.utils import secure_filename
-from data_processing import files_handling, processing
+from data_processing import processing
 
 
 class AppUi:
@@ -25,12 +27,21 @@ class AppUi:
         st.file_uploader(label = "Upload Your Signal File:", type=['csv'],
             on_change=self.change_signal_upload, key="signalUploader")
 
-        st.slider(label="Change your samlping rate: ", min_value= 0, max_value=100, \
+        st.slider(label="Change your samlping rate: ", min_value= 0, max_value=100,
             on_change=self.change_sampling_rate, key="signalSlider")
+
+    def show_error(self, errorMessage):
+        st.error(errorMessage)
+        
 
     def change_signal_upload(self):
         filePath = self.save_file(st.session_state.signalUploader)
         self.start_signal_drawing(filePath)
+
+        st.download_button(label="Save Signal", on_click=self.signalObject.saving_signal, data = self.signalObject.outputFile, 
+            file_name=self.signalObject.outputFileName, mime = 'csv', key="downloadButton")
+    
+       
 
     def save_file(self, csvFile):
         filePath = os.path.join(
@@ -38,15 +49,21 @@ class AppUi:
 
         with open(filePath, "wb") as file: 
             file.write(csvFile.getbuffer())
-            
+
         return filePath
 
     def start_signal_drawing(self, filePath):
-        self.signalDataFrame = files_handling.reading_signal(filePath)
-        self.signalObject.initialize_signal(self.signalDataFrame)
-        self.sampledSignal = self.signalObject.sample_signal()
-        # TODO: Signal Drawing.
+        try:
+            self.signalObject.reading_signal(filePath)
+            # self.sampledSignal = self.signalObject.sample_signal()
+            # TODO: Signal Drawing. 
+        except Exception as exceptionMessage:
+            self.show_error(exceptionMessage)
         
     def change_sampling_rate(self):
         self.sampledSignal = self.signalObject.sample_signal(st.session_state.signalSlider)
         # TODO: Signal Drawing.
+
+    def draw_signal(self, signalDataFrame):
+        fig = ff.create_distplot(signalDataFrame, bin_size=[.1, .25, .5])
+        st.plotly_chart(fig, use_container_width=True)
