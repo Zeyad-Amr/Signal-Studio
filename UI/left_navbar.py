9df3093eff38ceb4f5c367becd04c6fd4caa2b09
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2022-10-21 01:17:20 +0200

    modifying palette colors

--- UI/left_navbar.py
+++ UI/left_navbar.py
@@ -13,9 +13,6 @@ class leftNavBar:
 
         with st.form("signals"):
             submitted = st.form_submit_button("Add")
-            if submitted:
-                # TO-DO add signals
-                print('Add checked signals')
 
             st.checkbox("Signal 1")
             st.checkbox("Signal 2")
@@ -25,5 +22,3 @@ class leftNavBar:
             st.checkbox("Signal 6")
             st.checkbox("Signal 7")
             st.checkbox("Signal 8")
-
-
