b2a97eba5b4d6492a0a2c0a8b7f9752f3f84fea0
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2022-10-21 17:49:41 +0200

    separate sampling form elements

--- UI/right_navbar.py
+++ UI/right_navbar.py
@@ -13,38 +13,21 @@ class rightNavBar:
         """, unsafe_allow_html=True)
 
         # sampling
-        with st.form("sampling_form"):
-            slider_val = st.slider("Sampling")
-
-            # Every form must have a submit button.
-            submitted = st.form_submit_button("Reconstruct")
-            if submitted:
-                st.success("Reconstructed Successfully")
-
-        # signal file upload
-        uploadSignal = st.file_uploader("Upload Signal", type=["csv"])
-        if uploadSignal:
-            # TODO: Browse signal function
-            print("Browse signal function: ", uploadSignal)
-
-        # generate signal
-        with st.form("generate_signal"):
-            st.write("Generate Signal")
-            signalTitle = st.text_input("Signal Title")
-
-            freqVal = st.number_input("Frequency")
-            ampVal = st.number_input("Amplitude")
-            phaseVal = st.number_input("Phase")
-
-            submitted = st.form_submit_button("Generate")
-            if submitted:
-                # TODO: Generate signal function
-                print(signalTitle, ", ", freqVal, ", ", ampVal, ", ", phaseVal)
-                st.success("Generated Successfully")
         with st.container():
-            # add noise
-            st.write("Add Noise")
-            noiseSNR = st.slider("SNR")
-            if noiseSNR:
-                # TODO: SNR change function
-                    print("SNR change function: ", noiseSNR)
+            slider_val = st.slider("Sampling")
+            if slider_val:
+                print(slider_val)
+            st.button("Reconstruct", on_click=self.on_clicked)
+
+
+
+
+        # add noise
+        st.write("Add Noise")
+        noiseSNR = st.slider("SNR")
+        if noiseSNR:
+            # TODO: SNR change function
+            print("SNR change function: ", noiseSNR)
+
+    def on_clicked(self):
+        print("Clicked")
